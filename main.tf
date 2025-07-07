8d8d8772dc0e52a417ed16b3339cb42da8ec219f
juan.c.fernandez <juan.carlos.fernandez@ricoh.es>
2025-07-07 13:56:35 +0200

    update to version 4.0 of azurerm provider

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
   required_version = ">= 1.0"
@@ -10,4 +10,5 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "3e64aba9-b007-4ec9-b16f-4e5151f88cc6"
 }
--- network.tf
+++ network.tf
@@ -25,7 +25,8 @@ resource "azurerm_public_ip" "vm" {
   name                = "pip-srv-nddprint-activo"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
   domain_name_label   = "srv-nddprint-activo"
 }
 
